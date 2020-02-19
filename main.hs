3500b82ef787d500e18b0fadabb4b2c86ecd06d5
horrokotoj1337 <60961478+horrokotoj1337@users.noreply.github.com>
2020-02-19 11:57:45 +0100

    Update main.hs

--- main.hs
+++ main.hs
@@ -19,6 +19,10 @@ testBoard :: Board -> Board
 testBoard [] = [White Knight, Black Rook, Empty, Empty, White King]
 testBoard _ = [Empty]
 
+{- newBoard
+   Creates a new chessboard
+   Returns A list of Square where the first element in the list corresponds to A1 on a chess board, the 9th element corresponds to B1 on a chess board and thr 64th element corresponds to H8 on a chess board
+-}
 newBoard = [White Rook, White Knight, White Bishop, White Queen, White King, White Bishop, White Knight, White Rook, White Pawn, White Pawn, White Pawn, White Pawn, White Pawn, White Pawn, White Pawn, White Pawn, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Black Pawn, Black Pawn, Black Pawn, Black Pawn, Black Pawn, Black Pawn, Black Pawn, Black Pawn, Black Rook, Black Knight, Black Bishop, Black Queen, Black King, Black Bishop, Black Knight, Black Rook]
 
 
