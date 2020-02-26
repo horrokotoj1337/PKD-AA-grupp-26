028879d4bcf4e85f714bfe1998e0dfb3f740e6fc
horrokotoj1337 <60961478+horrokotoj1337@users.noreply.github.com>
2020-02-26 11:42:05 +0100

    move tar move
    move tar nu int istället för sträng
    validMovePawn ligger nu i Move.hs

--- Moves.hs
+++ Moves.hs
@@ -19,17 +19,17 @@ data Piece = Pawn | Knight | Bishop | Rook | Queen | King
 --main = undefined
 
 
-{- move b i o
-   moves a piece from the Square corresponding to i to the square corresponding to o on the board.. -- Moves a chess piece from one square to another.
+{- move b int1 int2
+   moves a piece from the Square corresponding to int1 to the square corresponding to int2 on the board.
    PRE: move need to be valid
-   RETURNS: a board where the piece on 'i' has been moved to 'o'.
-   EXAMPLES: move newBoard "b1" "c1" = [White Rook, White Knight, White Bishop, White Queen, White King, White Bishop, White Knight, White Rook, Empty, White Pawn, White Pawn, White Pawn, White Pawn, White Pawn, White Pawn, White Pawn, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Black Pawn, Black Pawn, Black Pawn, Black Pawn, Black Pawn, Black Pawn, Black Pawn, Black Pawn, Black Rook, Black Knight, Black Bishop, Black Queen, Black King, Black Bishop, Black Knight, Black Rook]
+   RETURNS: a board where the piece on 'int1' has been moved to 'int2'.
+   EXAMPLES: move newBoard 1 2 = [White Rook,Empty,White Pawn,Empty,Empty,Empty,Black Pawn,Black Rook,White Knight,White Pawn,Empty,Empty,Empty,Empty,Black Pawn,Black Knight,White Bishop,White Pawn,Empty,Empty,Empty,Empty,Black Pawn,Black Bishop,White Queen,White Pawn,Empty,Empty,Empty,Empty,Black Pawn,Black Queen,White King,White Pawn,Empty,Empty,Empty,Empty,Black Pawn,Black King,White Bishop,White Pawn,Empty,Empty,Empty,Empty,Black Pawn,Black Bishop,White Knight,White Pawn,Empty,Empty,Empty,Empty,Black Pawn,Black Knight,White Rook,White Pawn,Empty,Empty,Empty,Empty,Black Pawn,Black Rook]
 -}
-move :: Board -> String -> String -> Board
-move b i o = let
+move :: Board -> Int -> Int -> Board
+move b int1 int2 = let
               removed
-                = (take (position (convert i)) b) ++ [Empty] ++ (drop ((position (convert i)) + 1) b)
-             in (take (position (convert o)) removed) ++ ((onSquare b (position (convert i))) : (drop ((position (convert o)) + 1) b))
+                = (take int1 b) ++ [Empty] ++ (drop (int1 + 1) b)
+             in (take int2 removed) ++ ((onSquare b int1) : (drop (int2 + 1) b))
 
 {- convert i
    converts an input String into a pair of Int
@@ -101,9 +101,56 @@ isSameColour (White _) (White _) = True
 isSameColour (Black _) (Black _) = True
 isSameColour sq1 sq2             = False
 
+{- validMovePawn board (a, b) (c, d)
+   Checks whether it is valid to move a Pawn from (a, b) to (c, d)
+   Pre: (a, b) must be a Pawn. (a, b) and (c, d) must be Squares on the board
+   Returns: True if the move is valid, otherwise False
+   Example: validMovePawn newBoard (1, 2) (1, 3) = True
+            validMovePawn newBoard (1, 2) (1, 5) = False
+            validMovePawn newBoard (1, 7) (1, 6) = True
+-}
+
+validMovePawn :: Board -> (Int, Int) -> (Int, Int) -> Bool
+validMovePawn board (a, b) (c, d) | (isSameColour (onSquare board (position (a, b))) (onSquare board (position (c, d)))) = False
+                                  | a == c && ((onSquare board (position (c, d))) /= Empty) = False
+                                  | (isSameColour (onSquare board (position (a, b))) (White Pawn)) = validMoveWhitePawn board (a, b) (c, d)
+                                  | (isSameColour (onSquare board (position (a, b))) (Black Pawn)) = validMoveBlackPawn board (a, b) (c, d)
+                                  | otherwise = False
+
+
+{- validMoveWhitePawn board (a, b) (c, d)
+   Checks whether it is a valid to move a White Pawn from (a, b) to (c, d)
+   Pre: (a, b) must be a White Pawn
+   Returns True if the move is valid, otherwise False
+   Example: validMovePawn newBoard (1, 2) (1, 3) = True
+            validMovePawn newBoard (1, 2) (1, 5) = False
+-}
+validMoveWhitePawn :: Board -> (Int, Int) -> (Int, Int) -> Bool
+validMoveWhitePawn board (a, b) (c, d) | a == c && ((d - b == 1) || ((d - b == 2) && b == 2)) = True
+                                       | (abs (a - c)) == 1 && b < d && (onSquare board (position (c, d))) /= Empty = True
+                                       | otherwise = False
+                                       
+                                       
+
+{- validMoveBlackPawn board (a, b) (c, d)
+   Checks whether it is a valid to move a White Pawn from (a, b) to (c, d)
+   Pre: (a, b) must be a White Pawn
+   Returns True if the move is valid, otherwise False
+   Example: validMovePawn newBoard (1, 7) (1, 6) = True
+            validMovePawn newBoard (1, 7) (1, 4) = False
+-}
+validMoveBlackPawn :: Board -> (Int, Int) -> (Int, Int) -> Bool
+validMoveBlackPawn board (a, b) (c, d) | a == c && ((b - d == 1) || ((b - d == 2) && b == 7)) = True
+                                       | (abs (a - c)) == 1 && b > d && (onSquare board (position (c, d))) /= Empty = True
+                                       | otherwise = False
+
+
 
 {- newBoard -- This is a function, should be treated as one.
    Creates a new chessboard
    Returns A list of Square where the first element in the list corresponds to A1 on a chess board, the 9th element corresponds to B1 on a chess board and thr 64th element corresponds to H8 on a chess board
 -}
 newBoard = [White Rook, White Pawn, Empty, Empty, Empty, Empty, Black Pawn, Black Rook, White Knight, White Pawn, Empty, Empty, Empty, Empty, Black Pawn, Black Knight, White Bishop, White Pawn, Empty, Empty, Empty, Empty, Black Pawn, Black Bishop, White Queen, White Pawn, Empty, Empty, Empty, Empty, Black Pawn, Black Queen, White King, White Pawn, Empty, Empty, Empty, Empty, Black Pawn, Black King, White Bishop, White Pawn, Empty, Empty, Empty, Empty, Black Pawn, Black Bishop, White Knight, White Pawn, Empty, Empty, Empty, Empty, Black Pawn, Black Knight, White Rook, White Pawn, Empty, Empty, Empty, Empty, Black Pawn, Black Rook]
+
+
+whiterook = '♖'
